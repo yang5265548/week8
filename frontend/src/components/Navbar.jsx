7a2996a8147cd628adac29888c9b65a9c65fc8c6
Qingyun Wang <lucas.qingyun@gmail.com>
2024-10-07 11:31:28 +0300

    part 3 step 1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,15 +26,18 @@ const Navbar = () => {
         <h1>React Jobs</h1>
       </Link>
       <div className="links">
+        {isAuthenticated ? (
         <div>
           <Link to="/jobs/add-job">Add Job</Link>
-          {/* {email && <span>{email}</span>} */}
+          {email && <span>{email}</span>}
           <button onClick={handleClick}>Log out</button>
         </div>
+        ) : (
         <div>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
         </div>
+        )}
       </div>
       <button onClick={toggleTheme}>Toggle</button>
     </nav>
